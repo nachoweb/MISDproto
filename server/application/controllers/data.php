89a049057324190799f71b09cc8ba2ae56a2bfc9
Nachoweb <nachoelg@gmail.com>
2012-05-28 18:33:11 +0200

    first commit

---
+++ server/application/controllers/data.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Data extends CI_Controller {
+
+	public function index()
+	{
+		echo "Index of data";
+	}
+}
+
+/* End of file data.php */
+/* Location: ./application/controllers/data.php */
